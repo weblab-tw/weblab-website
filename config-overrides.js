886b85ca6338539b94e0616b71dfb0f792a1a037
徐愷 <kkshyu.tw@gmail.com>
2021-01-05 03:54:46 +0800

    [FEATURE] add theme colors

--- config-overrides.js
+++ config-overrides.js
@@ -13,7 +13,27 @@ module.exports = override(
   addLessLoader({
     lessOptions: {
       javascriptEnabled: true,
-      modifyVars: { ...defaultThemeVars, '@primary-color': '#b52' },
+      modifyVars: {
+        ...defaultThemeVars,
+        '@btn-danger-bg': '@primary-color',
+        '@btn-danger-border': '@primary-color',
+        '@btn-danger-color': '#fff',
+        '@btn-primary-bg': '@primary-color',
+        '@btn-primary-color': '#fff',
+        '@error-color': '#ff7d62',
+        '@heading-1-size': '40px',
+        '@heading-2-size': '32px',
+        '@heading-3-size': '24px',
+        '@heading-4-size': '20px',
+        '@heading-5-size': '18px',
+        '@heading-color': '#585858',
+        '@normal-color': '#585858',
+        '@primary-color': '#fd9998',
+        '@processing-color': 'rgba(255, 87, 96, 0.1)',
+        '@text-color': '#4a4a4a',
+        '@text-color-secondary': '#cdcdcd',
+        '@warning-color': '@primary-color',
+      },
     },
   }),
 )
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import Application from 'lodestar-app/src/Application'
 import React from 'react'
 import './App.scss'
-import HomePage from './pages/HomePage'
 
 const App = () => {
-  return (
-    <Application
-      appId="demo"
-      extraRouteProps={{
-        home: {
-          path: '/',
-          pageName: <HomePage />,
-          authenticated: false,
-        },
-      }}
-    />
-  )
+  return <Application appId="weblab" />
 }
 
 export default App
